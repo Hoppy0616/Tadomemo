0e8d331f2d5ee6476b15ce76c365f0ac651e7a6e
Hoppy0616 <hodahoda.20430@gmail.com>
2025-09-05 23:51:59 +0900

    chore(pwa): manifest を追加し Content-Type ヘッダを設定（開発用）

---
+++ next.config.mjs
@@ -0,0 +1,29 @@
+const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  images: {
+    unoptimized: true,
+  },
+  experimental: {
+    webpackBuildWorker: true,
+  },
+  async headers() {
+    return [
+      {
+        source: '/manifest.json',
+        headers: [
+          {
+            key: 'Content-Type',
+            value: 'application/manifest+json',
+          },
+        ],
+      },
+    ]
+  },
+}
+
+export default nextConfig
